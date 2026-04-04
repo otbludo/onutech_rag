c858ead12d146ae183b3f8264c95077a5cd0ccff
otbludo <otabelaludovic6@gmail.com>
2026-04-04 09:07:54 +0100

    mise a jour

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -175,7 +175,7 @@ const HomeScreen = () => {
           </div>
         )}
       </div>
-      <div className="fixed z-10 bottom-0 left-0 w-full p-4 md:p-6 bg-gradient-to-t from-slate-100 via-slate-100/90 to-transparent">
+      <div className="fixed z-12 bottom-0 left-0 w-full p-4 md:p-6 bg-gradient-to-t from-slate-100 via-slate-100/90 to-transparent">
         <div className="max-w-5xl mx-auto">
           <PromptInput
             value={prompt}
