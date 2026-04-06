3521a3efa672e1622051f9db5f4feb601d1e8e8a
otbludo <otabelaludovic6@gmail.com>
2026-04-06 18:56:49 +0100

    mise a jour extention du fichier app et hindex

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
-import App from './App.jsx'
+import App from './App.js'
 
 const queryClient = new QueryClient()
 
